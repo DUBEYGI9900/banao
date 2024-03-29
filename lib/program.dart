2f617006ebb57acc41bcec526a9d045dc2475109
shivam dubey <138605331+DUBEYGI9900@users.noreply.github.com>
2024-03-29 19:38:12 +0530

    Update program.dart

--- lib/program.dart
+++ lib/program.dart
@@ -1,40 +1,213 @@
 
 
-class Programs {
-  final String title;
-  final String description;
-  final String image;
-  final String text;
-  // final String? icon; // Optional icon
-  // final String? button; // Optional button
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
-  Programs({
-    required this.title,
-    required this.description,
-    required this.image,
-    required this.text,
-    // this.icon, // Optional icon
-    // this.button, // Optional button
+
+class Item {
+  DateTime createdAt;
+  String name;
+  String category;
+  int lesson;
+  String id;
+
+  Item({
+    required this.createdAt,
+    required this.name,
+    required this.category,
+    required this.lesson,
+    required this.id,
   });
 }
 
-class OnboardingItems {
-  List<Programs> items = [
-    Programs(
-      title: "LIFESTYLE",
-      description: "A complete guide for your new born baby",
-      image: "assets/images/Annotation1.jpg",
-      text: "16 lessons",
-     // Example of how to add a button
-    ),
-    Programs(
-      title: "WORKING PARENTS",
-      description: "Understanding of human behaviour",
-      image: "assets/images/Annotation2.jpg",
-      text: "12 lessons",
-      // You can choose not to include icon and button for this item
-    ),
-  ];
+class Programs extends StatefulWidget {
+  const Programs({Key? key}) : super(key: key);
 
+  @override
+  _ProgramsState createState() => _ProgramsState();
 }
 
+class _ProgramsState extends State<Programs> {
+  List<Item> itemsList = [];
+  bool showAllItems = false;
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+        Uri.parse('https://632017e19f82827dcf24a655.mockapi.io/api/programs'));
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      setState(() {
+        itemsList = (jsonData['items'] as List)
+            .map((item) => Item(
+                  createdAt: DateTime.parse(item['createdAt']),
+                  name: item['name'],
+                  // createdAt: item['cre'],
+                  category: item['category'],
+                  lesson: item['lesson'],
+                  id: item['id'],
+                ))
+            .toList();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Programs for you",
+                overflow: TextOverflow.ellipsis,
+                style: GoogleFonts.lora(
+                  fontSize: 18,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                width: 100,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    showAllItems = !showAllItems;
+                  });
+                },
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'View all',
+                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    SizedBox(width: 8),
+                    Icon(Icons.arrow_forward,
+                        color: Color.fromARGB(255, 0, 0, 0)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 12.0,
+                      mainAxisSpacing: 12.0,
+                      mainAxisExtent: 360,
+                    ),
+                    itemCount: showAllItems
+                        ? itemsList.length
+                        : (itemsList.length > 2 ? 2 : itemsList.length),
+                    itemBuilder: (context, index) {
+                      final item = itemsList[index];
+                      return Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: const Color.fromRGBO(235, 237, 240, 1),
+                          ),
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                topRight: Radius.circular(16),
+                              ),
+                              child: Image.asset(
+                                "assets/images/Annotation1.jpg",
+                                width: double.infinity,
+                                height: 200,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item.name,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Color.fromRGBO(89, 139, 237, 1),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    item.category,
+                                    style: const TextStyle(
+                                      color: Color.fromARGB(255, 0, 0, 0),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 30),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '${item.lesson} Lesson',
+                                        style: const TextStyle(
+                                          color: Color.fromARGB(255, 0, 0, 0),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                      //   Icon(CupertinoIcons.padlock)
+                                      // if (item.locked) // Display lock icon only if item is locked
+                                      //   Icon(CupertinoIcons.padlock)
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
