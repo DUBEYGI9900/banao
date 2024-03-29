a21d05dfa4bc538f47a5d366bb7930bc007a3d2b
shivam dubey <138605331+DUBEYGI9900@users.noreply.github.com>
2024-03-29 19:35:52 +0530

    Update Lessons.dart

--- lib/Lessons.dart
+++ lib/Lessons.dart
@@ -1,38 +1,205 @@
 
- class lessons {
-  final String title;
-  final String description;
-  final String image;
-  final String text;
-  // final String? icon; // Optional icon
-  // final String? button; // Optional button
 
-  lessons({
-    required this.title,
-    required this.description,
-    required this.image,
-    required this.text,
-    // this.icon, // Optional icon
-    // this.button, // Optional button
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+
+class Items {
+  DateTime createdAt;
+  String name;
+  int duration;
+  String category;
+  bool locked;
+  String id;
+
+  Items({
+    required this.createdAt,
+    required this.name,
+    required this.duration,
+    required this.category,
+    required this.locked,
+    required this.id,
   });
 }
- class you {
-  List<lessons> itemss = [
-    lessons(
-      title: "BABYCARE",
-      description: "U of human behaviour",
-      image: "assets/images/events1.jpg",
-      text: "3 min",
-     // Example of how to add a button
-    ),
-    lessons(
-      title: "BABYCARE",
-      description: "Understanding of human behaviour",
-      image: "assets/images/events1.jpg",
-      text: "1 min",
-     // Example of how to add a button
-    ),
-  ];
 
+class Lesson extends StatefulWidget {
+  const Lesson({Key? key}) : super(key: key);
+
+  @override
+  _LessonState createState() => _LessonState();
 }
 
+class _LessonState extends State<Lesson> {
+  List<Items> itemsList = [];
+  bool showAllItems = false;
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('https://632017e19f82827dcf24a655.mockapi.io/api/lessons'));
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      setState(() {
+        itemsList = (jsonData['items'] as List)
+            .map((item) => Items(
+          createdAt: DateTime.parse(item['createdAt']),
+          name: item['name'],
+          duration: item['duration'],
+          category: item['category'],
+          locked: item['locked'],
+          id: item['id'],
+        ))
+            .toList();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+        Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Lessons for you",
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.lora(
+                      fontSize: 18,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(width: 100,),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        showAllItems = !showAllItems;
+                      });
+                    },
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'View all',
+                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                        SizedBox(width: 8),
+                        Icon(Icons.arrow_forward, color: Color.fromARGB(255, 0, 0, 0)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 12.0,
+                      mainAxisSpacing: 12.0,
+                      mainAxisExtent: 360,
+                    ),
+                    itemCount: showAllItems ? itemsList.length : (itemsList.length > 2 ? 2 : itemsList.length),
+                    itemBuilder: (context, index) {
+                      final item = itemsList[index];
+                      return Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: const Color.fromRGBO(235, 237, 240, 1),
+                          ),
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                topRight: Radius.circular(16),
+                              ),
+                              child: Image.asset(
+                                "assets/images/events1.jpg",
+                                width: double.infinity,
+                                height: 200,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item.name,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Color.fromRGBO(89, 139, 237, 1),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    item.category,
+                                    style: const TextStyle(
+                                      color: Color.fromARGB(255, 0, 0, 0),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+const SizedBox(height: 30),                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '${item.duration} min',
+                                        style: const TextStyle(
+                                          color: Color.fromARGB(255, 0, 0, 0),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                      Icon(CupertinoIcons.padlock)
+                                      // if (item.locked) // Display lock icon only if item is locked
+                                      //   Icon(CupertinoIcons.padlock)
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
